b8ac03e513930d6ff342884f25ffafb189686add
Danil Nepomnyashih <nepomnachihd@gmail.com>
2021-05-09 12:23:31 +0600

    Fixed loader

--- src/store/slices/loader.ts
+++ src/store/slices/loader.ts
@@ -2,10 +2,16 @@ import {createSlice, PayloadAction} from '@reduxjs/toolkit';
 
 export const loader = createSlice({
   name: 'loader',
-  initialState: {isLoading: false},
+  initialState: {isLoading: false, counter: 0},
   reducers: {
     loading(state, action: PayloadAction<boolean>) {
-      state.isLoading = action.payload;
+      if (action.payload) {
+        state.counter++;
+      } else {
+        state.counter--;
+      }
+
+      state.isLoading = state.counter ? true : false;
     },
   },
 });
